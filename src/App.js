d235f21f336bb17a03b85c2e6cf576f597b067c7
Jay Yasay <paolo_yasay@yahoo.com>
2023-01-27 13:13:13 +0400

    Working form and added Ant Design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form'
+import TestForm from './components/Form'
 function App() {
 
   // const [data, setData] = useState([])
@@ -16,26 +15,7 @@ function App() {
   // }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Form />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* {data.map(item => (
-            <div key={item.id}>
-              <p>{item.name}</p>
-            </div>
-        ))} */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TestForm />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,113 @@
 import axios from "axios";
 import { useState } from "react";
+import { Button, Form, Input } from "antd";
+import { Card } from "antd";
+import { Col, Row } from "antd";
+import { Typography } from "antd";
 
-function Form() {
-    const [formData, setFormData] = useState({
-        name: '',
-        age: ''
+function TestForm() {
+  const { Title } = Typography;
+
+  const formStyle = {
+    input: {
+      textAlign: "left",
+    },
+  };
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+      [event.target.age]: event.target.value,
     });
+  };
 
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-            [event.target.age]: event.target.value
-        })
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/api/nameages", formData)
+      .then((res) => {
+        setFormData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setFormData({
+      name: "",
+      age: "",
+    });
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        axios.post('http://localhost:3001/api/nameages', formData)
-        .then(res => {
-            setFormData(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-        setFormData({
-            name: '',
-            age: ''
-        });
-    }
-
-    return (
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" name="name" onChange={handleChange} value={formData.name} />
-                </label>
-                <label>
-                    Age:
-                    <input type="number" name="age" onChange={handleChange} value={formData.age} />
-                </label><br />
-                <button type="submit">Add Details</button>
-            </form>
-  )
+  return (
+    <Row justify="center">
+      <Col>
+        <Title>Input your products</Title>
+        <Card
+          style={{
+            width: 500,
+          }}
+        >
+          <Form
+            labelCol={{
+              span: 3,
+            }}
+            wrapperCol={{
+              span: 24,
+            }}
+            style={{
+              maxWidth: "100%",
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onSubmit={handleSubmit}
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your name!",
+                },
+              ]}
+              style={formStyle.input}
+            >
+              <Input
+                placeholder="input placeholder"
+                onChange={handleChange}
+                value={formData.name}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Age"
+              name="age"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your age!",
+                },
+              ]}
+              style={formStyle.input}
+            >
+              <Input
+                placeholder="input placeholder"
+                onChange={handleChange}
+                value={formData.age}
+              />
+            </Form.Item>
+            <Button type="primary" htmlType="submit" size="medium">
+              Submit
+            </Button>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
 }
 
-export default Form
+export default TestForm;
