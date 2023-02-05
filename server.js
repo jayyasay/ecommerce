0e06f815f2074d4aab227db24918d887436d7e3b
unknown <paolo_yasay@yahoo.com>
2023-02-05 18:57:13 +0400

    Added authorization

--- server.js
+++ server.js
@@ -4,72 +4,137 @@ const app = express();
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
+
 const ImageModel = require("./image.model");
+const RegistrationModel = require("./registration.model");
+const {
+    log
+} = require("console");
 
-mongoose.connect("mongodb://localhost:27017/db", { useNewUrlParser: true });
-
-const NameAgeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
+mongoose.connect("mongodb://localhost:27017/db", {
+    useNewUrlParser: true
 });
 
 const Storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, uniquePrefix + '-' + file.originalname)
-  },
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, uniquePrefix + '-' + file.originalname)
+    },
 });
 
 const upload = multer({
-  storage: Storage,
+    storage: Storage,
 });
 
-const NameAge = new mongoose.model("NameAge", NameAgeSchema);
+app.post("/api/db/login", (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    RegistrationModel.findOne({
+        email: email
+    }, (error, user) => {
+        if (error) {
+            return res.status(500).send("Error in the server");
+        }
+        if (!user) {
+            return res.status(404).send("No user found");
+        }
 
-app.post("/api/db/nameage", (req, res) => {
-  const nameage = new NameAge(req.body);
-  nameage.save().then((result) => {
-    res.json({ message: "Product save successfully", nameage: result });
-  });
+        console.log(
+            user
+        );
+        bcrypt.compare(password, user.password, (err, result) => {
+            console.log(user.password);
+            console.log(result);
+            if (err) {
+                return res.status(500).send({
+                    auth: true,
+                    error: "Error in the server",
+                    token: null
+                });
+            }
+            if (!result) {
+                return res.status(401).send({
+                    auth: false,
+                    error: "Incorrect email or password",
+                    token: null
+                });
+            }
+            const token = jwt.sign({
+                id: user._id
+            }, config.get("secret"), {
+                expiresIn: 86400
+            });
+            res.status(200).send({
+                auth: true,
+                token: token
+            });
+        });
+    });
+});
+
+app.post("/api/db/registrations", (req, res) => {
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).send("Error in the server");
+        }
+        const registration = new RegistrationModel({
+            username,
+            email,
+            password: hash,
+        });
+        registration.save().then(() => res.send("Registration successful!"));
+    });
 });
 
 app.post("/api/db/products", upload.single("itemImage"), (req, res, next) => {
-  const products = new ImageModel({
-    itemName: req.body.itemName,
-    itemDesc: req.body.itemDesc,
-    itemQuantity: req.body.itemQuantity,
-    itemImage: {
-      data: fs.readFileSync(req.file.path),
-      contentType: req.file.mimetype,
-    },
-  });
-  products.save().then(() => res.send("Successfully uploaded!"));
+    const products = new ImageModel({
+        itemName: req.body.itemName,
+        itemDesc: req.body.itemDesc,
+        itemQuantity: req.body.itemQuantity,
+        itemImage: {
+            data: fs.readFileSync(req.file.path),
+            contentType: req.file.mimetype,
+        },
+    });
+    products.save().then(() => res.send("Successfully uploaded!"));
 });
 
-app.get("/api/db/nameage", (req, res) => {
-  NameAge.find().then((details) => {
-    res.json(details);
-  });
+app.get("/api/db/login", (req, res) => {
+    RegistrationModel.find().then((details) => {
+        res.json(details);
+    });
+});
+
+app.get("/api/db/registrations", (req, res) => {
+    RegistrationModel.find().then((details) => {
+        res.json(details);
+    });
 });
 
 app.get("/api/db/products", (req, res) => {
-  ImageModel.find().then((details) => {
-    res.json(details);
-  });
+    ImageModel.find().then((details) => {
+        res.json(details);
+    });
 });
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+    console.log("Server is running on port 3001");
 });
---
+++ src/components/Login.js
@@ -0,0 +1,147 @@
+import axios from "axios";
+import { useState } from "react";
+import { Button, Form, Input, message } from "antd";
+import { Card } from "antd";
+import { Col, Row } from "antd";
+import { Typography } from "antd";
+
+function LoginForm() {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const { Title } = Typography;
+
+  const [form] = Form.useForm();
+
+  const formStyle = {
+    input: {
+      textAlign: "left",
+    },
+  };
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onFinish = () => {
+    const data = {
+      email: formData.email,
+      password: formData.password,
+    };
+
+    axios
+      .post("http://localhost:3001/api/db/login", data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer secret",
+        },
+      })
+      .then((res) => {
+        if (res.data.auth) {
+          localStorage.setItem("token", res.data.token);
+          messageApi.open({
+            type: "success",
+            content: "Login successfully",
+          });
+        } else {
+          messageApi.open({
+            type: "error",
+            content: "Incorrect username or password",
+          });
+        }
+      })
+      .catch((err) => {
+        messageApi.open({
+          type: "error",
+          content: "Error logging in, please try again",
+        });
+      });
+    form.resetFields();
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Row justify="center">
+        <Col>
+          <Title> Login </Title>
+          <Card
+            style={{
+              width: 500,
+            }}
+          >
+            <Form
+              form={form}
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 24,
+              }}
+              style={{
+                maxWidth: "100%",
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item
+                label="E-mail"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your email",
+                  },
+                ]}
+                style={formStyle.input}
+              >
+                <Input
+                  name="email"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                  value={formData.name}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your correct password",
+                  },
+                ]}
+                style={formStyle.input}
+              >
+                <Input.Password
+                  name="password"
+                  placeholder="Enter password"
+                  onChange={handleChange}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" size="medium">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default LoginForm;
