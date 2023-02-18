d4cd63cc4ef14290b88fcce10fe16d3e72232c14
unknown <paolo_yasay@yahoo.com>
2023-02-18 22:17:01 +0400

    Final development for cart-page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'antd/dist/reset.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "antd/dist/reset.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -14,6 +14,7 @@ import {
   Tag,
   Skeleton,
   Spin,
+  Tooltip,
 } from "antd";
 import { useNavigate } from "react-router-dom";
 import BreadCrumb from "../components/Breadcrumb";
@@ -32,6 +33,8 @@ function ProductPage() {
   const [quantityOptions, setQuantityOptions] = useState([]);
   const [selectedQuantity, setSelectedQuantity] = useState(1);
   const [spin, setSpin] = useState(false);
+  const [disabledSelect, setDisabledSelect] = useState(false);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
   const { cartItems, setCartItems } = useContext(CartContext);
 
   const productList = () => {
@@ -76,6 +79,12 @@ function ProductPage() {
     },
   ];
 
+  const dataLocalStorage = localStorage.getItem("cartItems");
+  const dataLocalStorageParsed = JSON.parse(dataLocalStorage);
+  const matchingItem = dataLocalStorageParsed.find(
+    (item) => item.id === fetchProduct._id
+  );
+
   const handleAddToCart = () => {
     setSpin(true);
     setTimeout(() => {
@@ -91,8 +100,14 @@ function ProductPage() {
         (cartItem) => cartItem.id === item.id
       );
 
-      const existingItemQuantity = cartItems.map(
-        (existingItem) => item.quantity + existingItem.quantity
+      // const existingItemQuantity = cartItems.map(
+      //   (existingItem) => item.quantity + existingItem.quantity
+      // );
+
+      const existingItemQuantity = cartItems.reduce(
+        (total, existingItem) =>
+          existingItem.id === item.id ? total + existingItem.quantity : total,
+        0
       );
 
       if (existingItemIndex > -1) {
@@ -114,24 +129,42 @@ function ProductPage() {
           i <= fetchProduct.itemQuantity - newCartItemsQuantity;
           i++
         ) {
-          options.push({ value: i, label: `${i}` });
+          options.push({
+            value: i,
+            label: `${i}`,
+          });
         }
         setQuantityOptions(options);
         setSelectedQuantity(
-          parseInt(existingItemQuantity) === fetchProduct.itemQuantity
-            ? "Out of stock"
+          existingItemQuantity + selectedQuantity === fetchProduct.itemQuantity
+            ? setDisabledSelect(true)
             : 1
         );
+        setTooltipVisible(
+          existingItemQuantity + selectedQuantity === fetchProduct.itemQuantity
+            ? true
+            : false
+        );
+        console.log(existingItemQuantity);
       } else {
         setCartItems([...cartItems, item]);
         const options = [];
         for (let i = 1; i <= fetchProduct.itemQuantity - item.quantity; i++) {
-          options.push({ value: i, label: `${i}` });
+          options.push({
+            value: i,
+            label: `${i}`,
+          });
         }
         setQuantityOptions(options);
         setSelectedQuantity(
-          item.quantity === fetchProduct.itemQuantity ? "Out of stock" : 1
+          item.quantity === fetchProduct.itemQuantity
+            ? setDisabledSelect(true)
+            : 1
         );
+        setTooltipVisible(
+          item.quantity === fetchProduct.itemQuantity ? true : false
+        );
+        console.log("Not Existing!");
       }
     }, 500);
   };
@@ -142,21 +175,40 @@ function ProductPage() {
 
   useEffect(() => {
     if (fetchProduct.itemQuantity) {
+      const quantity = matchingItem ? matchingItem.quantity : 0; // set quantity to 0 if no matching item is found
       const options = [];
-      for (let i = 1; i <= fetchProduct.itemQuantity; i++) {
-        options.push({ value: i, label: `${i}` });
+      console.log("Quantity for ID " + fetchProduct._id + ": " + quantity);
+      for (let i = 1; i <= fetchProduct.itemQuantity - quantity; i++) {
+        options.push({
+          value: i,
+          label: `${i}`,
+        });
       }
       setQuantityOptions(options);
+      setSelectedQuantity(
+        quantity === fetchProduct.itemQuantity ? setDisabledSelect(true) : 1
+      );
+      setTooltipVisible(quantity === fetchProduct.itemQuantity ? true : false);
     }
   }, [fetchProduct.itemQuantity]);
 
   return (
     <>
-      <Row style={{ maxWidth: "1200px", margin: "auto" }}>
-        <Row gutter={[0, 40]} style={{ marginTop: 20, marginBottom: 20 }}>
+      <Row
+        style={{
+          maxWidth: "1200px",
+          margin: "auto",
+        }}
+      >
+        <Row
+          gutter={[0, 40]}
+          style={{
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        >
           <BreadCrumb itemName={fetchProduct.itemName} />
         </Row>
-
         <Row gutter={[40, 8]} justify="center">
           <Col span={10}>
             {fetchProduct.itemImage && (
@@ -176,47 +228,68 @@ function ProductPage() {
             )}
           </Col>
           <Col span={14}>
-            <Title>{fetchProduct.itemName}</Title>
-            <Divider />
+            <Title> {fetchProduct.itemName} </Title> <Divider />
             <Title level={2}>
-              ₱ {fetchProduct.itemPrice}{" "}
-              <small style={{ fontSize: 12 }}>tax included</small>
+              ₱{fetchProduct.itemPrice}
+              <small
+                style={{
+                  fontSize: 12,
+                }}
+              >
+                tax included
+              </small>
             </Title>
             <Divider />
-            <Title level={3}>Quantity</Title>
+            <Title level={3}> Quantity </Title>
             <Spin tip="Loading..." spinning={spin}>
-              <Space direction="vertical" size="middle">
-                <Select
-                  value={selectedQuantity}
-                  style={{
-                    width: 120,
-                  }}
-                  onChange={(value) => setSelectedQuantity(value)}
-                  options={quantityOptions}
-                />
-                <Button type="primary" size="small" onClick={handleAddToCart}>
-                  Add to cart
-                </Button>
-              </Space>
+              <Tooltip
+                title="You've already selected the maximum available quantity"
+                open={tooltipVisible}
+              >
+                <Space direction="vertical" size="middle">
+                  <Select
+                    value={selectedQuantity}
+                    style={{
+                      width: 120,
+                    }}
+                    onChange={(value) => setSelectedQuantity(value)}
+                    options={quantityOptions}
+                    disabled={disabledSelect}
+                  />
+
+                  <Button
+                    disabled={disabledSelect}
+                    type="primary"
+                    size="small"
+                    onClick={handleAddToCart}
+                  >
+                    Add to cart
+                  </Button>
+                </Space>
+              </Tooltip>
             </Spin>
             <Divider />
-            <Title level={3}>Description</Title>
-            <Text>{fetchProduct.itemDesc}</Text>
-            <Divider />
+            <Title level={3}> Description </Title>
+            <Text> {fetchProduct.itemDesc} </Text> <Divider />
             <Row>
               <Col span={12}>
-                <Title level={3}>Material</Title>
-                <Text>{fetchProduct.itemMaterial}</Text>
+                <Title level={3}> Material </Title>
+                <Text> {fetchProduct.itemMaterial} </Text>
               </Col>
               <Col span={12}>
-                <Title level={3}>Finish</Title>
-                <Text>{fetchProduct.itemFinish}</Text>
+                <Title level={3}> Finish </Title>
+                <Text> {fetchProduct.itemFinish} </Text>
               </Col>
             </Row>
           </Col>
-          <Row style={{ marginTop: 20, width: "100%" }}>
+          <Row
+            style={{
+              marginTop: 20,
+              width: "100%",
+            }}
+          >
             <Col span={24}>
-              <Title level={4}>Dimensions</Title>
+              <Title level={4}> Dimensions </Title>
               <Table pagination={false} columns={columns} dataSource={data} />
             </Col>
           </Row>
