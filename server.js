5cadbacf66ff004c1db3a384830873138d553d9b
unknown <paolo_yasay@yahoo.com>
2023-02-21 20:34:30 +0400

    Removal of credentials

--- server.js
+++ server.js
@@ -26,13 +26,10 @@ const RegistrationModel = require("./registration.model");
 // );
 
 mongoose
-  .connect(
-    "mongodb+srv://jayyasay:BoyBoy0322@cluster0.qjies0v.mongodb.net/db",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect("mongodb://localhost:27017/db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((error) => console.error(error));
 
